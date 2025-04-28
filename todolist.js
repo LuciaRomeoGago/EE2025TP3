9685b528c5db2fb86f6daa65e4abd66f158169f3
Lucia Romeo Gago <luciaromeogago@hotmail.com>
2025-04-28 20:47:16 -0300

    Primer commit, falta prioridad y modificar tareas

---
+++ todolist.js
@@ -0,0 +1,60 @@
+/*Creo boton para eliminar en cada lista*/
+var lista = document.getElementsByTagName("li"); /*devuelve coleccion de elemtnos li del documento*/
+for (let i = 0; i < lista.length; i++) { /*se crea span y nodo de texto x y se le agrega a span cerrar, ambos al final de cada li*/
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "cerrar";
+  span.appendChild(txt);
+  lista[i].appendChild(span);
+}
+
+// apretar cerrar boton para ocultar un item de la lista
+var cerrarIniciales = document.getElementsByClassName("cerrar");
+for (let i = 0; i < cerrarIniciales.length; i++) {
+  cerrarIniciales[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+//anadir "un tick" cuando apreto en el item
+var listo = document.querySelector('ul');
+listo.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('tachado');
+  }
+}, false);
+
+// creo un item nuevo cuando aprieto el boton de agregar
+function nuevoItem() {
+  var li = document.createElement("li");
+  var inputValor = document.getElementById("toDo-input").value;
+  var t = document.createTextNode(inputValor);
+  li.appendChild(t);
+  if (inputValor === '') {
+    alert("Ingresar alguna tarea por hacer!");
+  } else {
+    document.getElementById("toDo-ul").appendChild(li);
+  }
+  document.getElementById("toDo-input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "cerrar";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  // Adjuntar el event listener al botón "cerrar" recién creado
+  span.onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+function modificarItem(){
+
+}
+
+function listarPrioridades(){
+
+}
